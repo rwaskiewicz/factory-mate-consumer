e09de38fb1095bfb6f7cf956f955c81146f40f57
ryan.waskiewicz <ryanwaskiewicz@gmail.com>
2017-08-06 17:40:21 -0400

    Fix naming of constructor injected service

--- src/services/CartBuilderService.ts
+++ src/services/CartBuilderService.ts
@@ -3,14 +3,14 @@ import { GroceryItemFactory } from '../domain/GroceryItemFactory';
 import { GroceryItemBuilderService } from './GroceryBuilderService';
 
 export class CartBuilderService {
-    constructor(public cartBuilderService: GroceryItemBuilderService) { }
+    constructor(public groceryItemBuilderService: GroceryItemBuilderService) { }
 
     public buildRandomCart() {
         const numberOfItemsToBuild = 10;
         const cart: GroceryItem[] = [];
 
         for (let i = 0; i < numberOfItemsToBuild; i++) {
-            cart.push(this.cartBuilderService.retrieveGroceryItem());
+            cart.push(this.groceryItemBuilderService.retrieveGroceryItem());
         }
         return cart;
     }
