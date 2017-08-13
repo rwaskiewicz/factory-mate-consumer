d507346df4a14adef9c894b7865cff2d04bfba1e
ryan.waskiewicz <ryanwaskiewicz@gmail.com>
2017-08-13 10:57:37 -0400

    Updates for 1.2.0 release
    - Organize CartBuilderService spec with additional describe blocks
    - Update GroceryItemFactory to use ProvidedValueGenerator

--- src/domain/GroceryItemFactory.ts
+++ src/domain/GroceryItemFactory.ts
@@ -1,15 +1,27 @@
-import { FactoryMate, FactoryMateAware } from 'factory-mate';
-import { NumberGenerator } from 'factory-mate';
+import { FactoryMate, FactoryMateAware, NumberGenerator, ProvidedValueGenerator } from 'factory-mate';
 import { GroceryItem } from './GroceryItem';
 
 @FactoryMateAware
 export class GroceryItemFactory {
     public define() {
         const numberGenerator = new NumberGenerator();
+        const providedValueGenerator = new ProvidedValueGenerator([
+            'chewy cookies',
+            'fresh cookies',
+            'fresh ice cream',
+            'melty chocolate',
+            'sweet ice cream',
+            'delicious cookies',
+            'melty chocolate',
+            'crispy cookies',
+            'fresh crackers',
+            'melty nougat'
+        ]);
+
         FactoryMate.define(GroceryItem, (): GroceryItem => {
             const groceryItem = new GroceryItem();
             groceryItem.id = numberGenerator.nextValue();
-            groceryItem.groceryName = 'chewy cookies';
+            groceryItem.groceryName = providedValueGenerator.nextValue();
             return groceryItem;
         });
     }
--- src/services/CartBuilderService.spec.ts
+++ src/services/CartBuilderService.spec.ts
@@ -7,49 +7,54 @@ describe('CartBuilderService', () => {
   let mockGroceryBuilderService;
 
   beforeAll(() => {
-    mockGroceryBuilderService = jasmine.createSpyObj('GrooceryBuilderService', ['retrieveGroceryItem']);
+    mockGroceryBuilderService = jasmine.createSpyObj('GroceryBuilderService', ['retrieveGroceryItem']);
 
     cartBuilderService = new CartBuilderService(mockGroceryBuilderService);
   });
 
-  it('builds a random cart of size 10', () => {
-    const cart = cartBuilderService.buildRandomCart();
+  describe('Default building', () => {
+    it('builds a random cart of size 10', () => {
+      const cart = cartBuilderService.buildRandomCart();
 
-    expect(cart.length).toBe(10);
+      expect(cart.length).toBe(10);
+    });
+
+    it('receives an item with the name chewy cookies', () => {
+      const groceryItem: GroceryItem = FactoryMate.build(GroceryItem.name);
+      mockGroceryBuilderService.retrieveGroceryItem.and.returnValue(groceryItem);
+
+      const cart = cartBuilderService.buildRandomCart();
+
+      expect(cart[0].groceryName).toBe('chewy cookies');
+    });
   });
 
-  it('receives an item with the name chewy cookies', () => {
-    const groceryItem: GroceryItem = FactoryMate.build(GroceryItem.name);
-    mockGroceryBuilderService.retrieveGroceryItem.and.returnValue(groceryItem);
+  describe('Overriding properties', () => {
+    it('overrides an item with the name chunky cookies and an ID of 23', () => {
+      const groceryItem: GroceryItem = FactoryMate.build(GroceryItem.name,
+        (u: GroceryItem) => {
+          u.id = 23;
+          u.groceryName = 'soft cookies';
+        });
+      mockGroceryBuilderService.retrieveGroceryItem.and.returnValue(groceryItem);
 
-    const cart = cartBuilderService.buildRandomCart();
+      const cart = cartBuilderService.buildRandomCart();
 
-    expect(cart[0].groceryName).toBe('chewy cookies');
+      expect(cart[0].id).toBe(23);
+      expect(cart[0].groceryName).toBe('soft cookies');
+    });
   });
 
-  it('overrides an item with the name chunky cookies and an ID of 23', () => {
-    const groceryItem: GroceryItem = FactoryMate.build(GroceryItem.name,
-      (u: GroceryItem) => {
-        u.id = 23;
-        u.groceryName = 'chunky cookies';
+  describe('Building many', () => {
+    it('builds many of the same item', () => {
+      const groceryItems: GroceryItem[] = FactoryMate.buildMany(GroceryItem.name, 3);
+
+      expect(groceryItems.length).toBe(3);
+
+      let expectedId = 3;
+      groceryItems.forEach((groceryItem: GroceryItem) => {
+        expect(groceryItem.id).toBe(expectedId++);
       });
-    mockGroceryBuilderService.retrieveGroceryItem.and.returnValue(groceryItem);
-
-    const cart = cartBuilderService.buildRandomCart();
-
-    expect(cart[0].id).toBe(23);
-    expect(cart[0].groceryName).toBe('chunky cookies');
-  });
-
-  it('builds many of the same item', () => {
-    const groceryItems: GroceryItem[] = FactoryMate.buildMany(GroceryItem.name, 3);
-
-    expect(groceryItems.length).toBe(3);
-    expect(groceryItems[2]).not.toBeUndefined();
-
-    let expectedId = 3;
-    groceryItems.forEach((groceryItem: GroceryItem) => {
-      expect(groceryItem.id).toBe(expectedId++);
     });
   });
 });
